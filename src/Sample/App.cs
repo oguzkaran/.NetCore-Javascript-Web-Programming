60aa1cd0b1ba967812d3a4793a9ea7033b741539
Oğuz Karan <oguzkaran@csystem.org>
2021-08-26 21:12:21 +0300

    Update

--- src/Sample/App.cs
+++ src/Sample/App.cs
@@ -1,5 +1,5 @@
 ﻿/*---------------------------------------------------------------------------------------------------------------------
-    Lambda ifadeleri içerisinde kendisinden önce bildirilen yerel ve parametre değişkenleri doğrudan kullanılabilir (capture)
+    var anahtar sözcüğünün bir kullanımı
 ----------------------------------------------------------------------------------------------------------------------*/
 using System;
 using System.Collections.Generic;
@@ -10,47 +10,15 @@ namespace CSD
     {
         public static void Main()
         {
-            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            List<int> result = new List<int>();
-            List<int> evens = new List<int>();
+            dynamic a = 10;
 
-            Console.Write("Bir sayı giriniz:");
-            int a = int.Parse(Console.ReadLine());
+            a = "ankara";
 
-            Algorithm.Transform(numbers, result, val => val * a);            
-            Algorithm.ForEach(result, val => Console.Write("{0} ", val));
-            Console.WriteLine();
-            Algorithm.Filter(numbers, evens, val => val % 2 == 0);
-            Algorithm.ForEach(evens, val => Console.Write("{0} ", val));
-            Console.WriteLine();
+            a = new Random();
+
+            //...
         }
-    }    
-
-    static class Algorithm 
-    {
-        public static int Filter<T>(List<T> src, List<T> dest, Func<T, bool> pred)
-        {
-            int count = 0;
-
-            foreach (var t in src)
-                if (pred(t))
-                {
-                    ++count;
-                    dest.Add(t);
-                }
-
-            return count;
-        }
-        public static void Transform<T>(List<T> src, List<T> dest, Func<T, T> func)
-        {
-            foreach (var t in src)
-                dest.Add(func(t));
-        }
-
-        public static void ForEach<T>(List<T> list, Action<T> action)
-        {
-            foreach (var t in list)
-                action(t);
-        }
-    }    
+    }
 }
+
+
