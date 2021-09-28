993aacb2cd853c6fe458f643c50d9ef6630c1bd0
Oğuz Karan <oguzkaran@csystem.org>
2021-09-28 22:53:14 +0300

    Update

--- src/DotnetCore/Projects/ASPDotnet/WebAPI/003-TodoApplicationRestAppMultiLayer/db.sql
+++ src/DotnetCore/Projects/ASPDotnet/WebAPI/003-TodoApplicationRestAppMultiLayer/db.sql
@@ -6,3 +6,5 @@ create table TodoInfo (
 	LastUpdate datetime default(SYSDATETIME()) not null,
 	Completed bit default(0) not null	
 )
+
+insert into TodoInfo (Title, Text) values ('Project', 'Repository')
