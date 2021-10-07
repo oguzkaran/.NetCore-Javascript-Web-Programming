2b48f5cbad619efe6f2f512ba32448813a887808
Oğuz Karan <oguzkaran@csystem.org>
2021-10-07 21:49:58 +0300

    Update

---
+++ src/DotnetCore/Projects/ASPDotnet/WebAPI/MovieRestServiceApplication/Db.sql
@@ -0,0 +1,32 @@
+create database MovieAppDb
+
+go
+
+use MovieAppDb
+
+go
+
+create table Movie (
+	Id bigint primary key identity(1, 1),
+	Name nvarchar(256) not null,
+    SceneDate date not null,
+	Rating bigint default(0),
+	Imdb real default(0.0),
+	Cost money not null
+)
+
+go
+
+create table Director (
+	Id int primary key identity(1, 1),
+	Name nvarchar(256) not null,
+	BirthDate Date not null,
+)
+
+go
+
+create table MovieToDirector (
+	Id bigint primary key identity(1, 1),
+	MovieId bigint foreign key references Movie(Id) not null,
+	DirectorId int foreign key references Director(Id) not null
+)
--- src/DotnetCore/Projects/ASPDotnet/WebAPI/MovieRestServiceApplication/Program.cs
+++ src/DotnetCore/Projects/ASPDotnet/WebAPI/MovieRestServiceApplication/Program.cs
@@ -1,7 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace _001_TodoApplicationRestApp
+namespace MovieRestServiceApplication
 {
     public class Program
     {
