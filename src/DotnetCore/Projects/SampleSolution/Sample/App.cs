a778e6047130c6a5deb5435a8b9b3821bb1bc212
Oğuz Karan <oguzkaran@csystem.org>
2021-10-14 21:01:27 +0300

    Update

--- src/DotnetCore/Projects/SampleSolution/Sample/App.cs
+++ src/DotnetCore/Projects/SampleSolution/Sample/App.cs
@@ -1,69 +1,36 @@
 ﻿/*---------------------------------------------------------------------------------------------------------------------
-    Single Responbility Principle
-    Open Closed Principle
-    Liskov Substitution Principle
-    Interface Segregation Principle
-    Dependency Inverse Principle 
-
-    Dependency Injection (Inversion Of Control)
+    Task sınıfı
 ----------------------------------------------------------------------------------------------------------------------*/
 using System;
-using System.Collections.Generic;
-using CSD.Util.Collections;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CSD
 {
-    interface ISource {
-        int NextChar();
-    }
-
-    class StringSource : ISource {
-        private readonly string m_str;
-        private int m_idx;
-
-        public StringSource(string str)
-        {
-            m_str = str;
-        }
-
-        public int NextChar()
-        {
-            return m_idx == m_str.Length ? -1 : m_str[m_idx++];
-        }
-    }
-
-    class Parser {
-        public ISource Source { get; set; }
-
-        public Parser(ISource source)
-        {
-            Source = source;
-        }
-
-        public int CountWhitespaces()
-        {
-            int count = 0;
-
-            int c;
-
-            while ((c = Source.NextChar()) != -1)
-                if (char.IsWhiteSpace((char)c))
-                    ++count;
-
-            return count;
-        }
-    }
-
-
     class App
     {
         public static void Main()
         {
-            StringSource ss = new("Bugün hava \t\nyağmurlu");
-            Parser p = new(ss);
+            var mt = new Sample { Ch = '.', Milliseconds = 1000};
+            var t = new Task(() => mt.PrintChar(10));
+            
+            t.Start();
 
+            Console.WriteLine("Main ends!...");
+            Console.ReadKey();
+        }
+    }
 
-            Console.WriteLine($"Count:{p.CountWhitespaces()}");
+    class Sample {
+        public char Ch { get; set; }        
+        public int Milliseconds { get; set; }
+
+        public void PrintChar(int n)
+        {
+            for (int i = 0; i < n; ++i) {
+                Console.Write($"{Ch}");
+                Thread.Sleep(Milliseconds);
+            }
         }
     }
 }
