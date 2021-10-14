56a2e81640e426b0e81befe5f74fe4c416588697
Oğuz Karan <oguzkaran@csystem.org>
2021-10-14 22:27:52 +0300

    Update

--- src/DotnetCore/Projects/SampleSolution/Sample/App.cs
+++ src/DotnetCore/Projects/SampleSolution/Sample/App.cs
@@ -1,7 +1,8 @@
 ﻿/*---------------------------------------------------------------------------------------------------------------------
-    Task sınıfı
+    Yukarıdaki problem async belirleyicisi ve await operatörü ile daha kolay bir biçimde yazılabilir
 ----------------------------------------------------------------------------------------------------------------------*/
 using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,26 +12,38 @@ namespace CSD
     {
         public static void Main()
         {
-            var mt = new Sample { Ch = '.', Milliseconds = 1000};
-            var t = new Task(() => mt.PrintChar(10));
-            
-            t.Start();
-
-            Console.WriteLine("Main ends!...");
+            Console.WriteLine("main starts");
+            Sample.FooAsync();
+            Console.WriteLine("main ends");
             Console.ReadKey();
         }
     }
 
     class Sample {
-        public char Ch { get; set; }        
-        public int Milliseconds { get; set; }
-
-        public void PrintChar(int n)
+        public static async void FooAsync()
         {
-            for (int i = 0; i < n; ++i) {
-                Console.Write($"{Ch}");
-                Thread.Sleep(Milliseconds);
+            var task = new Task<int>(MyTaskUtil.MyTaskProc);
+
+            task.Start();            
+
+            Console.WriteLine($"Sum:{await task}"); // Burada çağıran için metot sonlanır. await'ten sonraki kısım başka bir thread'de devam eder
+        }
+    }
+
+    class MyTaskUtil {
+        public static int MyTaskProc()
+        {
+            Random r = new();
+            int sum = 0;
+
+            for (int i = 0; i < 5; ++i) {
+                int val = r.Next(100);
+
+                Thread.Sleep(500);
+                sum += val;
             }
+
+            return sum;
         }
     }
 }
