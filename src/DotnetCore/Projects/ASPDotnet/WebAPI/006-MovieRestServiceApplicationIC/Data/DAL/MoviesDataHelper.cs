c56ced8c0c7fa0c0e2a57953d7141373e3bf87fc
Oğuz Karan <oguzkaran@csystem.org>
2021-11-16 22:30:09 +0300

    Update

--- src/DotnetCore/Projects/ASPDotnet/WebAPI/006-MovieRestServiceApplicationIC/Data/DAL/MoviesDataHelper.cs
+++ src/DotnetCore/Projects/ASPDotnet/WebAPI/006-MovieRestServiceApplicationIC/Data/DAL/MoviesDataHelper.cs
@@ -1,4 +1,8 @@
-﻿using CSD.MovieRestServiceApplication.Data.Repositories;
+﻿using CSD.MovieRestServiceApplication.Data.Entities;
+using CSD.MovieRestServiceApplication.Data.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static CSD.Data.DatabaseUtil;
 
 namespace CSD.MovieRestServiceApplication.DAL
 {
@@ -13,6 +17,46 @@ namespace CSD.MovieRestServiceApplication.DAL
             m_directorRepository = directorRepository;
         }
 
+        #region Movie
+        public Task<IEnumerable<Movie>> FindMoviesByYearAsync(int year)
+        {
+            return SubscribeRepositoryAsync(() => m_movieRepository.FindByYearAsync(year), "MoviesDataHelper.FindMoviesByYearAsync");
+        }
 
+        public Task<IEnumerable<Movie>> FindMoviesByYearAndMonthAsync(int year, int month)
+        {
+            return SubscribeRepositoryAsync(() => m_movieRepository.FindByYearAndMonthAsync(year, month), "MoviesDataHelper.FindMoviesByYearAndMonthAsync");
+        }
+
+        public Task<IEnumerable<Movie>> FindMoviesByDirectorIdAsync(int id)
+        {
+            return SubscribeRepositoryAsync(() => m_movieRepository.FindByDirectorIdAsync(id), "MoviesDataHelper.FindMoviesByDirectorIdAsync");
+        }
+
+        public Task<Movie> SaveMovieAsync(Movie movie)
+        {
+            return SubscribeRepositoryAsync(() => m_movieRepository.SaveAsync(movie), "MoviesDataHelper.SaveMovieAsync");
+        }
+        //...
+        #endregion
+
+        #region Director
+        public Task<IEnumerable<Director>> FindDirectorByAgeGreaterAsync(double threshold)
+        {
+            return SubscribeRepositoryAsync(() => m_directorRepository.FindByAgeGreaterAsync(threshold), "MoviesDataHelper.FindDirectorByAgeGreaterAsync");
+        }
+
+        public Task<IEnumerable<Director>> FindDirectorByAgeLessAsync(double threshold)
+        {
+            return SubscribeRepositoryAsync(() => m_directorRepository.FindByAgeLessAsync(threshold), "MoviesDataHelper.FindDirectorByAgeLessAsync");
+        }
+
+        public Task<Director> SaveDirectorAsync(Director director)
+        {
+            return SubscribeRepositoryAsync(() => m_directorRepository.SaveAsync(director), "MoviesDataHelper.SaveDirectorAsync");
+        }
+
+        //...
+        #endregion
     }
 }
