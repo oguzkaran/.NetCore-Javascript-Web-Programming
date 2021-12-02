0935f73ada012c5f78a46d7ad9eb4fe086633395
Oğuz Karan <oguzkaran@csystem.org>
2021-12-02 20:54:39 +0300

    Update

---
+++ src/DotnetCore/Projects/ASPDotnet/WebAPI/007-WikiSearchAppGeonames/WikiSearchAppRepositoryLib/IWikiSearchRepository.cs
@@ -0,0 +1,12 @@
+﻿using CSD.WikiSearchApp.Data.Repositories.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSD.WikiSearchApp.Data.Repositories
+{
+    public interface IWikiSearchRepository
+    {
+        Task<WikiSearch> Save(WikiSearch wikiSearch);
+        Task<IEnumerable<WikiSearch>> FindByQ(string q);
+    }
+}
---
+++ src/DotnetCore/Projects/ASPDotnet/WebAPI/007-WikiSearchAppGeonames/WikiSearchAppRepositoryLib/WikiSearchRepository.cs
@@ -0,0 +1,44 @@
+﻿using CSD.WikiSearchApp.Data.Repositories.Contexts;
+using CSD.WikiSearchApp.Data.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CSD.Util.TPL.TaskUtil;
+
+namespace CSD.WikiSearchApp.Data.Repositories
+{
+    public class WikiSearchRepository : IWikiSearchRepository
+    {
+        private readonly WikiSearchAppDbContext m_wikiSearchAppDbContext;
+
+        private IEnumerable<WikiSearch> findByQCallback(string q)
+        {
+            return m_wikiSearchAppDbContext.WikiSearches.Where(ws => ws.Q == q);
+        }
+
+        private WikiSearch savecallback(WikiSearch wikiSearch)
+        {
+            m_wikiSearchAppDbContext.WikiSearches.Add(wikiSearch);
+            m_wikiSearchAppDbContext.SaveChanges();
+
+            return wikiSearch;
+        }
+
+        public WikiSearchRepository(WikiSearchAppDbContext wikiSearchAppDbContext)
+        {
+            m_wikiSearchAppDbContext = wikiSearchAppDbContext;
+        }
+
+        public Task<IEnumerable<WikiSearch>> FindByQ(string q)
+        {
+            return Create(() => findByQCallback(q));            
+        }
+
+        public Task<WikiSearch> Save(WikiSearch wikiSearch)
+        {
+            return Create(() => savecallback(wikiSearch));
+        }
+    }
+}
