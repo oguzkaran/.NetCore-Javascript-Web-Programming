e7d869d8a73b795b0ee21ed96ffee739a8cde1e4
Oğuz Karan <oguzkaran@csystem.org>
2021-12-14 19:33:01 +0300

    Update

--- src/DotnetCore/Projects/ASPDotnet/WebAPI/007-WikiSearchAppGeonames/WikiSearchServiceGeoNames/Controllers/WikiSearchController.cs
+++ src/DotnetCore/Projects/ASPDotnet/WebAPI/007-WikiSearchAppGeonames/WikiSearchServiceGeoNames/Controllers/WikiSearchController.cs
@@ -1,12 +1,9 @@
-﻿using CSD.WikiSearchApp.Geonames;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CSD.WikiSearchApp.Controllers
-{       
+{
     [Route("api/[controller]")]
     public class WikiSearchController : ControllerBase
     {
