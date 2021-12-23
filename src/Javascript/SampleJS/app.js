03b4f5de74590771996fa9b53aea710f6d389bd2
Oğuz Karan <oguzkaran@csystem.org>
2021-12-23 22:30:55 +0300

    Update

--- src/Javascript/SampleJS/app.js
+++ src/Javascript/SampleJS/app.js
@@ -1,33 +1,54 @@
-/*
-Sınıf Çalışması: Parametresi ile aldığı number türden bir sayının basamak sayısını döndüren digitsCount fonksiyonunu
-yazınız
- */
 function writeln(a)
 {
     console.log(a)
 }
 
-function digitsCount(val)
+function isPrime(val)
 {
-    if (!val)
-        return 1
+    if (val <= 1)
+        return false
 
+    if (val % 2 == 0)
+        return val == 2
+
+    if (val % 3 == 0)
+        return val == 3
+
+    if (val % 5 == 0)
+        return val == 5
+
+    if (val % 7 == 0)
+        return val == 7
+
+    for (let i = 11; i * i<= val; i += 2)
+        if (val % i == 0)
+            return false
+
+    return true
+}
+
+function getPrime(n)
+{
     let count = 0
+    let val = 2
 
-    while (val) {
-        ++count;
-        val = parseInt(val / 10)
+    for (;;) {
+        if (isPrime(val))
+            ++count
+
+        if (count == n)
+            return val
+
+        ++val
     }
-
-    return count
 }
 
 function main()
 {
-    writeln(digitsCount(0))
-    writeln(digitsCount(-123))
-    writeln(digitsCount(123))
-    writeln(digitsCount("ankara"))
+    writeln(getPrime(1))
+    writeln(getPrime(2))
+    writeln(getPrime(3))
+    writeln(getPrime(20))
 }
-main()
 
+main()
