f1acdd4c5d2ba9e512b4367754c84aadd8cd55a2
Oğuz Karan <oguzkaran@csystem.org>
2022-01-06 22:36:06 +0300

    Update

--- src/Javascript/SampleJS/app.js
+++ src/Javascript/SampleJS/app.js
@@ -1,47 +1,14 @@
-function write(a)
-{
-    process.stdout.write(a)
-}
-
 function writeln(a)
 {
-    write(a === undefined ? '\n' : `${a}\n`)
+    console.log(a)
 }
-
-function randomInt(min, max)
-{
-    return Math.floor(Math.random() * (max - min)) + min + 1
-}
-
-function countValue(array, value)
-{
-    let count = 0
-
-    array.forEach(e => {if (e === value) ++count})
-
-    return count
-}
-
-function generateRandomArray(count, min, max)
-{
-    let array = []
-
-    for (let i = 0; i < count; ++i)
-        array[i] = randomInt(min, max)
-
-    return array
-}
-
 function main()
 {
-    let a = generateRandomArray(40, -10, 10)
+    let numbers = [1, 21, 30, 41, 5]
 
-    let count = countValue(a, 0)
-
-    writeln(a)
-
-    writeln(`Count:${count}`)
-}
+    if (numbers.some(val => val % 2 === 0))
+        writeln("En az bir tane çift sayı var")
+    else
+        writeln("Hiç çift yok")}
 
 main()
-
