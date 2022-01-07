6d76bff86587bf96698732fe3fe2baa0e9e9c3a2
Oğuz Karan <oguzkaran@csystem.org>
2022-01-07 09:27:49 +0300

    Update

--- src/Javascript/SampleJS/app.js
+++ src/Javascript/SampleJS/app.js
@@ -1,14 +1,43 @@
+function write(a)
+{
+    process.stdout.write(a)
+}
+
 function writeln(a)
 {
-    console.log(a)
+    write(a === undefined ? '\n' : `${a}\n`)
 }
+
+let Product = function (name, price, stock)
+{
+    this.name = name
+    this.price = price
+    this.stock = stock
+    this.getTotal =  function () {return this.stock * this.price }
+}
+
 function main()
 {
-    let numbers = [1, 21, 30, 41, 5]
+    let products = new Array()
 
-    if (numbers.some(val => val % 2 === 0))
-        writeln("En az bir tane çift sayı var")
-    else
-        writeln("Hiç çift yok")}
+    products.push(new Product("laptop", 4000, 0))
+    products.push(new Product("mouse", 40, 349))
+    products.push(new Product("klavye", 30, 0))
+    products.push(new Product("kalem", 40, 34))
 
+    let productsNotInStock = new Array(products.length)
+
+    let index = -1
+
+    while (true) {
+        index = products.findIndex((p, i) => i >= index + 1 && p.stock <= 0)
+
+        if (index === -1)
+            break
+
+        productsNotInStock.push(products[index])
+    }
+
+    productsNotInStock.forEach(p => writeln(p.name))
+}
 main()
