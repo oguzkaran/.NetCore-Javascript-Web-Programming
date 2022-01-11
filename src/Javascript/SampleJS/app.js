843e1d7fc7ae1501edfb5e920eb28e8520d46fbe
Oğuz Karan <oguzkaran@csystem.org>
2022-01-11 20:58:09 +0300

    Update

--- src/Javascript/SampleJS/app.js
+++ src/Javascript/SampleJS/app.js
@@ -8,36 +8,102 @@ function writeln(a)
     write(a === undefined ? '\n' : `${a}\n`)
 }
 
-let Product = function (name, price, stock)
+function randomInt(min, max)
 {
-    this.name = name
-    this.price = price
+    return Math.floor(Math.random() * (max - min) + min + 1)
+}
+
+function randomDouble(min, max)
+{
+    return Math.random() * (max - min) + min
+}
+
+function randomText(n, text)
+{
+    let str = ""
+    let len = text.length;
+
+    for (let i = 0; i < n; ++i)
+        str += text.charAt(randomInt(0, len))
+
+    return str
+}
+
+function randomTextTR(n)
+{
+    return randomText(n, "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZabcçdefgğhıijklmnoöprsştuüvyz")
+}
+
+
+function randomTextsTR(n, min, max)
+{
+    let result = [];
+
+    while (n--)
+        result.push(randomTextTR(randomInt(min, max)))
+
+    return result
+}
+
+
+function randomTextEN(n)
+{
+    return randomText(n, "ABCDEFGHIJKLMNOPQRSTUWXVYZabcdefghijklmnopqrstuwxvyz")
+}
+
+function randomTextsEN(n, min, max)
+{
+    let result = [];
+
+    while (n--)
+        result.push(randomTextEN(randomInt(min, max)))
+
+    return result
+}
+
+let Product = function (id, name, stock, cost, price) {
+    this.id = id
+    this.name = name;
     this.stock = stock
-    this.getTotal =  function () {return this.stock * this.price }
+    this.cost = cost
+    this.price = price
+    this.getTotal = function () {return this.stock * this.price}
+    this.getTotalCost = function () {return this.stock * this.cost}
+    this.toString = function () {
+        return JSON.stringify(this)
+    }
+}
+
+function createRandomProduct(id)
+{
+    let name = randomTextTR(randomInt(5, 15))
+    let stock = randomInt(-100, 100)
+    let cost = randomDouble(34.7, 109.45)
+    let price = randomDouble(34.7, 109.45)
+
+    return new Product(id, name, stock, cost, price)
+}
+
+function createRandomProducts(n)
+{
+    let products = []
+
+    for (let i = 1; i <= n; ++i)
+        products.push(createRandomProduct(i))
+
+    return products
 }
 
 function main()
 {
-    let products = new Array()
+    let products = createRandomProducts(10)
 
-    products.push(new Product("laptop", 4000, 0))
-    products.push(new Product("mouse", 40, 349))
-    products.push(new Product("klavye", 30, 0))
-    products.push(new Product("kalem", 40, 34))
+    products.forEach(writeln)
+    writeln("-------------------------")
+    products.filter(p => p.stock > 0).sort((p1, p2) => p2.price - p1.price).forEach(writeln)
+    writeln("-------------------------")
+    products.filter(p => p.stock < 0).sort((p1, p2) => p1.getTotal()- p2.getTotal()).forEach(writeln)
 
-    let productsNotInStock = new Array(products.length)
-
-    let index = -1
-
-    while (true) {
-        index = products.findIndex((p, i) => i >= index + 1 && p.stock <= 0)
-
-        if (index === -1)
-            break
-
-        productsNotInStock.push(products[index])
-    }
-
-    productsNotInStock.forEach(p => writeln(p.name))
 }
+
 main()
