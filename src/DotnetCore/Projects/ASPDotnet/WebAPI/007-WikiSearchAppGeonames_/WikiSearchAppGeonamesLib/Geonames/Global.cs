01693dbd2252bc823d430e873529dee994bc9269
Oğuz Karan <oguzkaran@csystem.org>
2022-04-27 16:46:07 +0300

    Update

--- src/DotnetCore/Projects/ASPDotnet/WebAPI/007-WikiSearchAppGeonames_/WikiSearchAppGeonamesLib/Geonames/Global.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CSD.WikiSearchApp.Geonames
-{
-    static class Global
-    {
-        public const string WikiUrlTemplate = "http://api.geonames.org/wikipediaSearchJSON?q={0}&maxRows={1}&username=csystem";
-    }
-}
